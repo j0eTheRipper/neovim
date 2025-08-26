1c5bfc7e7dc572e2076113d623060b95c38f555d
j0eTheRipper <j0etheripper0010@gmail.com>
2025-08-27 01:31:21 +0800

    nvim configs

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    opts = {
+      position = "right",
+    },
+  },
+  {
+    "nvim-flutter/flutter-tools.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- optional for vim.ui.select
+    },
+    config = true,
+  },
+}
